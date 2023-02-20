e3673e49b771e3b1e71e6f5bb7d9f41cd203b48d
Moono25 <moonomapenzi@gmail.com>
2023-02-20 14:58:46 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,13 @@
  */
 int main(void)
 {
-    printf("The size of a char is %zu byte(s)\n", sizeof(char));
-    printf("The size of a short is %zu byte(s)\n", sizeof(short));
-    printf("The size of an int is %zu byte(s)\n", sizeof(int));
-    printf("The size of a long is %zu byte(s)\n", sizeof(long));
-    printf("The size of a long long is %zu byte(s)\n", sizeof(long long));
-    printf("The size of a float is %zu byte(s)\n", sizeof(float));
-    printf("The size of a double is %zu byte(s)\n", sizeof(double));
-    printf("The size of a long double is %zu byte(s)\n", sizeof(long double));
-    return 0;
+printf("The size of a char is %d byte(s)\n", sizeof(char));
+printf("The size of a short is %d byte(s)\n", sizeof(short));
+printf("The size of an int is %d byte(s)\n", sizeof(int));
+printf("The size of a long is %d byte(s)\n", sizeof(long));
+printf("The size of a long long is %d byte(s)\n", sizeof(long long));
+printf("The size of a float is %d byte(s)\n", sizeof(float));
+printf("The size of a double is %d byte(s)\n", sizeof(double));
+printf("The size of a long double is %d byte(s)\n", sizeof(long double));
+return 0;
 }
