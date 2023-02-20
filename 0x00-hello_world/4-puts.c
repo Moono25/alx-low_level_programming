ece4324084f7abf7a2f89dd05b2c49c2584cdb05
Moono25 <moonomapenzi@gmail.com>
2023-02-20 10:31:14 -0800

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
- 
-
 /**
- * main - Entry point
+ * main - A Program that uses the puts instead of printf
  *
  * Return: Always 0 (sucess)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	Returns (0);
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
