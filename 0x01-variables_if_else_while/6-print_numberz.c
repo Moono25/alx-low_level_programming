53fe854e734378e8b387d588d4161dd8661d1325
Moono25 <moonomapenzi@gmail.com>
2023-02-21 15:50:39 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Main - Entry
+ * Discription: "print base 10 digits  using the putchar function"
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for(n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
