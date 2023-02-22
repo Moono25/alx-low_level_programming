ae607b2bd3bea6428dbddd7daf0b42aa1d022180
Moono25 <moonomapenzi@gmail.com>
2023-02-21 16:50:32 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * Main - Entry point
+ * Description: "prints the lowercase alphabet in reverse"
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <=102; m++)
+	{
+		putchar(m);
+	}
+
+        putchar('\n');
+	return (0);
+}
