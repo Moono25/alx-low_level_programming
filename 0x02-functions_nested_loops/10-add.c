a778765aaaa131f7cc8d374d90f21c47074d5863
Moono25 <moonomapenzi@gmail.com>
2023-02-22 19:58:21 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Add two values
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
+ * Return: Integer value
+ */
+int add(int a, int b) 
+{
+    return a + b;
+}
